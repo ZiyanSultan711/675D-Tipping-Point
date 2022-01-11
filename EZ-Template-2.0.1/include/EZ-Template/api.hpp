70ba64042349bc971d924f6a6f2be3455390755e
ZiyanSultan711 <ziyanssultan@gmail.com>
2022-01-11 08:34:05 -0500

    EZ 2.0.1
    - all 1.0.2 op control and filing has been imported in
    - drive class cannot be accessed in src/675D/ -- temp fix by putting drive lock function inside main of main.cpp
    - need to adjust PID values and add auton routines with correct function naming

---
+++ EZ-Template-2.0.1/include/EZ-Template/api.hpp
@@ -0,0 +1,17 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#pragma once
+
+#include "EZ-Template/PID.hpp"
+#include "EZ-Template/auton.hpp"
+#include "EZ-Template/auton_selector.hpp"
+#include "EZ-Template/drive/drive.hpp"
+#include "EZ-Template/sdcard.hpp"
+#include "EZ-Template/util.hpp"
+
+#include "675D/auton_helper_functions.hpp"
+#include "675D/op_control_functions.hpp"
