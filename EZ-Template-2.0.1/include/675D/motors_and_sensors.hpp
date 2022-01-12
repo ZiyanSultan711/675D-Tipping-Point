e43f489f97dc6d026242364988336f067d31e5df
ZiyanSultan711 <ziyanssultan@gmail.com>
2022-01-12 13:29:36 -0500

    motor declaration organized

---
+++ EZ-Template-2.0.1/include/675D/motors_and_sensors.hpp
@@ -0,0 +1,10 @@
+#include "api.h"
+
+extern pros::Motor lift_l;
+extern pros::Motor lift_r;
+extern pros::Motor mogo;
+extern pros::Motor conveyor;
+
+extern pros::ADIDigitalOut piston_clamp;
+
+extern pros::Rotation mogo_sensor;
--- EZ-Template-2.0.1/include/EZ-Template/util.hpp
+++ EZ-Template-2.0.1/include/EZ-Template/util.hpp
@@ -17,15 +17,6 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
   */
 extern pros::Controller master;
 
-extern pros::Motor lift_l;
-extern pros::Motor lift_r;
-extern pros::Motor mogo;
-extern pros::Motor conveyor;
-
-extern pros::ADIDigitalOut piston_clamp;
-
-extern pros::Rotation mogo_sensor;
-
 namespace ez {
 
 #define DISABLE 0
