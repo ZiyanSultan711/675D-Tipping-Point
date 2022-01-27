5371051162768511232431a900156f3a09329b61
ZiyanSultan711 <ziyanssultan@gmail.com>
2022-01-27 10:20:45 -0500

    cleaning repo

--- EZ-Template-2.1.0/src/675D/motors_and_sensors.cpp
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-pros::Motor lift_l(4, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
-pros::Motor lift_r(5, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
-pros::Motor mogo(18, MOTOR_GEARSET_36, true, MOTOR_ENCODER_DEGREES);
-pros::Motor conveyor(8, MOTOR_GEARSET_6, false, MOTOR_ENCODER_DEGREES);
-
-pros::ADIDigitalOut piston_clamp('G', false);
